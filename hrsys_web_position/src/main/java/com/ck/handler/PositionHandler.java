7d63607d409a950d6a49731778275dd4c0d3e14d
liuD <2670573413@qq.com>
2021-04-13 18:44:55 +0800

    position 职位发布web

--- hrsys_web_position/src/main/java/com/ck/handler/PositionHandler.java
+++ hrsys_web_position/src/main/java/com/ck/handler/PositionHandler.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * created  on 2021/4/12 16:01
  * liu
@@ -22,6 +25,13 @@ public class PositionHandler {
     public String AddEngage_major_release(Engage_major_release engage_major_release){
         iEngage_major_release_service.AddEngage_major_release(engage_major_release);
         return  "forward:toPage?page=page/recruit/position/position_change_update.html";
+    }
+
+    @RequestMapping("QueryEngage_major_release")
+    public  String QueryEngage_major_release(Map map) {
+        List<Engage_major_release> engage_major_releases = iEngage_major_release_service.QueryEngage_major_release();
+        map.put("zhiWei", engage_major_releases);
+        return "forward:toPage?page=page/recruit/position/position_release_search.html";
     }
 
 
